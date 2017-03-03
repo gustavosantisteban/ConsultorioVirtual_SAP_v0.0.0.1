eb3ee67a64a8cce1d2d62312c860e3b229140dce
William Gustavo Santisteban <rootsantisteban@gmail.com>
2017-03-03 18:22:25 -0300

    Cambios Observer

---
+++ SeguridadWebv2/Models/Aplicacion/Observer/ConcreteObserver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace SeguridadWebv2.Models.Aplicacion.Observer
+{
+    public class ConcreteObserver : IObservable<HorarioDisponible>
+    {
+        public List<IObserver<HorarioDisponible>> horarioobservers;
+        public List<HorarioDisponible> contractservices;
+
+        public ConcreteObserver()
+        {
+            horarioobservers = new List<IObserver<HorarioDisponible>>();
+            contractservices = new List<HorarioDisponible>();
+        }
+
+        public IDisposable Subscribe(IObserver<HorarioDisponible> observer)
+        {
+            if (!horarioobservers.Contains(observer))
+            {
+                horarioobservers.Add(observer);
+                foreach (var item in contractservices)
+                    observer.OnNext(item);
+            }
+            return new Unsubscriber<HorarioDisponible>(horarioobservers, observer);
+        }
+
+        internal class Unsubscriber<ContractService> : IDisposable
+        {
+            private List<IObserver<ContractService>> _observers;
+            private IObserver<ContractService> _observer;
+
+            internal Unsubscriber(List<IObserver<ContractService>> observers, IObserver<ContractService> observer)
+            {
+                this._observers = observers;
+                this._observer = observer;
+            }
+
+            public void Dispose()
+            {
+                if (_observers.Contains(_observer))
+                    _observers.Remove(_observer);
+            }
+        }
+    }
+}
--- SeguridadWebv2/Models/Aplicacion/Turno.cs
+++ SeguridadWebv2/Models/Aplicacion/Turno.cs
@@ -8,8 +8,10 @@ using System.Web;
 namespace SeguridadWebv2.Models.Aplicacion
 {
     [Table("Turnos")]
-    public class Turno
+    public class Turno : IObserver<HorarioDisponible>
     {
+        private IDisposable unsubscriber;
+        private int intState;
         public Turno()
         {
             this.IdTurno = Guid.NewGuid().ToString();
@@ -26,6 +28,37 @@ namespace SeguridadWebv2.Models.Aplicacion
 
         [ForeignKey("RelacionId")]
         public virtual Relacion RelacionPacienteEspecialista { get; set; }
+
+        public Turno(int state)
+        {
+            this.intState = state;
+        }
+
+        public int State
+        {
+            get { return this.intState; }
+        }
+
+        public virtual void Suscribe(IObservable<HorarioDisponible> provider)
+        {
+            if (provider != null)
+                unsubscriber = provider.Subscribe(this);
+        }
+
+        public void OnNext(HorarioDisponible value)
+        {
+            string id = value.Disponible.ToString();
+        }
+
+        public void OnError(Exception error)
+        {
+            unsubscriber.Dispose();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public enum Estado
